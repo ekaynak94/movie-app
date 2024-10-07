7be506647fb1d1dbd43e6a4156d525f07331d59b
Emre Kaynak <emre@Emres-MacBook-Pro.local>
2024-10-07 22:15:04 +0300

    create global types file

--- api/movieService.ts
+++ api/movieService.ts
@@ -1,11 +1,4 @@
-// Define types for the movie data returned by the API
-interface Movie {
-  Title: string;
-  Year: string;
-  imdbID: string;
-  Type: string;
-  Poster: string;
-}
+import { Movie, MovieDetails } from "../types/movieTypes";
 
 interface MovieResponse {
   Search: Movie[];
@@ -14,34 +7,7 @@ interface MovieResponse {
   Error?: string;
 }
 
-interface MovieDetail {
-  Title: string;
-  Year: string;
-  Rated: string;
-  Released: string;
-  Runtime: string;
-  Genre: string;
-  Director: string;
-  Writer: string;
-  Actors: string;
-  Plot: string;
-  Language: string;
-  Country: string;
-  Awards: string;
-  Poster: string;
-  Ratings: {
-    Source: string;
-    Value: string;
-  }[];
-  Metascore: string;
-  imdbRating: string;
-  imdbVotes: string;
-  imdbID: string;
-  Type: string;
-  DVD: string;
-  BoxOffice: string;
-  Production: string;
-  Website: string;
+interface MovieDetailResponse extends MovieDetails {
   Response: string;
   Error?: string;
 }
@@ -76,7 +42,7 @@ export const getMovies = async (query: string): Promise<MovieResponse> => {
 // Function to fetch details of a single movie by its IMDb ID
 export const getMovieDetails = async (
   movieId: string
-): Promise<MovieDetail> => {
+): Promise<MovieDetailResponse> => {
   try {
     const response = await fetch(`${API_BASE_URL}?i=${movieId}&r=json`, {
       method: "GET",
@@ -90,7 +56,7 @@ export const getMovieDetails = async (
       throw new Error(`Error fetching movie details: ${response.statusText}`);
     }
 
-    const data: MovieDetail = await response.json();
+    const data: MovieDetailResponse = await response.json();
     return data;
   } catch (error) {
     console.error("Error fetching movie details:", error);
