5497eeb6b2d75f87b354d0f598787ff1427b3f91
Emre Kaynak <emre@Emres-MacBook-Pro.local>
2024-10-08 21:15:50 +0300

    implement ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,53 @@
-import { StyleSheet } from "react-native";
-import { ThemedView } from "@/components/ThemedView";
+import React, { useState } from "react";
+import { View, FlatList, StyleSheet } from "react-native";
+import { ThemedSafeAreaView } from "@/components/ThemedView";
+import MovieListItem from "@/components/MovieListItem";
+import { getMovies } from "@/api/movieService";
+import { Movie } from "@/types/movieTypes";
+import SearchBar from "@/components/SearchBar";
 
-export default function SearchScreen() {
-  return <ThemedView />;
-}
+const SearchScreen: React.FC = () => {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState<Movie[]>([]);
 
-const styles = StyleSheet.create({});
+  const searchMovies = async () => {
+    const results = await getMovies(query); // Fetch movies based on user input
+    setMovies(results.Search);
+  };
+
+  return (
+    <ThemedSafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <SearchBar
+          query={query}
+          onQueryChange={setQuery}
+          onSearch={searchMovies}
+        />
+        <FlatList
+          data={movies}
+          keyExtractor={(item) => item.imdbID}
+          renderItem={({ item }) => (
+            <MovieListItem
+              title={item.Title}
+              posterUrl={item.Poster}
+              onPress={() => {
+                console.log(`MOVIE Pressed: ${item.Title}`);
+              }}
+            />
+          )}
+        />
+      </View>
+    </ThemedSafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    paddingHorizontal: 16,
+  },
+});
+
+export default SearchScreen;
